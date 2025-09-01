70f4f8278af38a245652bbf80fa5105368691233
star145s <202779798+star145s@users.noreply.github.com>
2025-09-01 16:01:07 +0700

    Update __init__.py

--- constants/__init__.py
+++ constants/__init__.py
@@ -66,8 +66,8 @@ ALLOWED_MODEL_TYPES_1 = {
 
 # Emission distribution
 TRAINING_WEIGHT = 0
-CONFERENCE_WEIGHT = 0.2
-WORKSHOP_WEIGHT = 0.8
+CONFERENCE_WEIGHT = 0.0
+WORKSHOP_WEIGHT = 1.0
 
 # Synchronize on blocks roughly every 30 minutes.
 SYNC_BLOCK_CADENCE = 150
